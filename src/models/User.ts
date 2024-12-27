deb915c8222bc7b5aaed7662af7f6d8ea919dccd
JaviPeder~ <javierpedernera@gmail.com>
2024-12-27 20:41:36 -0300

    editar sucursal estilos responsivos- error al construir

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,22 @@
-import Status from "../redux/types/types";
+
+import { Status } from "../redux/types/types";
 import { Role } from "./RoleModel";
 
 export default interface User {
   user_id: number;
-  first_name: string; 
+  first_name: string;
   last_name: string;
-  country: string; 
-  city: string; 
-  birth_date: string; 
+  country: string;
+  city: string;
+  birth_date: string;
   email: string;
-  phone_number?: string; 
-  gender?: string; 
+  phone_number?: string;
+  gender?: string;
   subscribed_to_newsletter?: boolean;
-  status: Status ;
+  status: Status;
   token: string;
   image_url?: string;
-  exp:number
+  exp: number
   roles: Role[]
 }
 
